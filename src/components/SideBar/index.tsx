e530c9523ab59113786143ab4e8c000b4a26e933
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-07-13 15:33:07 -0300

    Add import new library and components

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import StickBox from 'react-sticky-box'
+
+import List from '../List'
+import FollowSuggestion from '../FollowSuggestion'
+import News from '../News'
 
 import { Container, SearchWrapper, SearchInput, SearchIcon, Body } from './styles'
 
@@ -10,9 +15,43 @@ const SideBar: React.FC = () => {
                 <SearchIcon />
             </SearchWrapper>
 
-            <Body>
-                <p>{'Lorem ipsum dolor sit amet. '.repeat(90)}</p>
-            </Body>
+            <StickBox>
+                <Body>
+                    <List 
+                        title="Talvez você curta"
+                        elements={[
+                            <FollowSuggestion 
+                                name="ReactJS"
+                                nickname="@reactjs"
+                            />,
+                            <FollowSuggestion 
+                                name="NodeJS"
+                                nickname="@nodejs"
+                            />,
+                            <FollowSuggestion 
+                                name="TypeScript"
+                                nickname="@typescript"
+                            />
+                        ]}
+                    />
+                    <List 
+                        title="Talvez você curta"
+                        elements={[<News />, <News />, <News />]}
+                    />
+                    <List 
+                        title="Talvez você curta"
+                        elements={[<News />, <News />, <News />]}
+                    />
+                    <List 
+                        title="Talvez você curta"
+                        elements={[<News />, <News />, <News />]}
+                    />
+                    <List 
+                        title="Talvez você curta"
+                        elements={[<News />, <News />, <News />]}
+                    />
+                </Body>
+            </StickBox>
         </Container>
     )
 }
--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -14,10 +14,48 @@ export const Container = styled.div`
 `;
 
 export const SearchWrapper = styled.div`
-    
+    padding: 10px 24px;
+    width: min(399px, 100%);
+
+    position: fixed;
+    top: 0;
+    z-index: 2;
+    background: var(--primary);
+
+    max-height: 57px;
 `;
 
-export const SearchInput = styled.input``;
+export const SearchInput = styled.input`
+    width: 100%;
+    height: 39px;
+    font-size: 14px;
+    padding: 0 10px 0 52px;
+    border-radius: 19.5px;
+    background: var(--search);
+
+    &::placeholder {
+        color: var(--gray);
+    }
+
+    ~ svg {
+        position: relative;
+        top: -33px;
+        left: 15px;
+        z-index: 1;
+
+        transition: 180ms ease-in-out;
+    }
+
+    outline: 0;
+
+    &:focus {
+        border: 1px solid var(--twitter);
+
+        ~ svg {
+            fill: var(--twitter);
+        }
+    }
+`;
 
 export const SearchIcon = styled(Search)`
     width: 27px;
@@ -26,4 +64,13 @@ export const SearchIcon = styled(Search)`
     fill: var(--gray);
 `;
 
-export const Body = styled.div``;
+export const Body = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 57px 24px 200px;
+    margin-top: 3px;
+
+    > div + div {
+        margin-top: 15px;
+    }
+`;
